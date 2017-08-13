4e2eb5bc67b6ddecf0075c7b3d56bd63879d872c
roin93 <Henrik@Henrik-Laptop.fritz.box>
2017-08-13 20:23:25 +0200

    Fixing some properties according to change of the compiler. Now using MinGW instead of something of Cygwin.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=gostatistics
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gostatistics
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/gostatistics
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=gostatistics.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gostatistics.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/gostatistics.tar
 # Release configuration
-CND_PLATFORM_Release=rpi-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/rpi-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=gostatistics
-CND_ARTIFACT_PATH_Release=dist/Release/rpi-Windows/gostatistics
-CND_PACKAGE_DIR_Release=dist/Release/rpi-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/gostatistics
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=gostatistics.tar
-CND_PACKAGE_PATH_Release=dist/Release/rpi-Windows/package/gostatistics.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/gostatistics.tar
 #
 # include compiler specific variables
 #
