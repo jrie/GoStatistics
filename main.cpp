8a15b6016090aab1f92d89d6cc463a57fb014041
roin93 <mail@henrik-viebrock.de>
2017-08-13 17:00:14 +0200

    Adding a small message when the program starts and has included the current available classes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <cstdlib>
+#include <iostream>
+
+#include "Coordinate.hpp"
+#include "Turn.hpp"
+#include "Group.hpp"
 
 using namespace std;
 
@@ -7,7 +12,7 @@ using namespace std;
  */
 int main(int argc, char** argv)
 {
-
+	cout << "The project is started but cannot do anything because there is not enough code to do something special..." << endl;
 	return 0;
 }
 
