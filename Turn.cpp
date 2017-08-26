5d5f9e140d44c7ebcbc68f8b57c59d9be8dd541f
roin93 <mail@henrik-viebrock.de>
2017-08-26 20:47:41 +0200

    Simplified Turn class by removing unecessary functions and improve Constructor

--- Turn.cpp
+++ Turn.cpp
@@ -5,27 +5,16 @@
  */
 int Turn::instances = 0;
 
-/**
- * @brief Standard Constructor
- */
-Turn::Turn()
-{
-	m_id = Turn::instances;
-	Turn::instances++;
-}
 
 /**
  * @brief Costum Constructor
  * @param color black = false, white = true
  * @param target Coordinate of the target field
  */
-Turn::Turn(bool color, Coordinate target)
+Turn::Turn(bool color, Coordinate target) : m_color(color), m_target(target), m_id(Turn::instances)
 {
-	m_id = Turn::instances;
+	//m_id = Turn::instances;
 	Turn::instances++;
-	setColor(color);
-	setTarget(target);
-	setInitialized();
 }
 
 /**
@@ -38,43 +27,12 @@ void Turn::setColor(bool color)
 }
 
 /**
- * @brief Sets the privat variables __initialized to true
- */
-void Turn::setInitialized()
-{
-	m_initialized = true;
-}
-
-/**
  * @brief Sets the target Coordinate
  * @param target The Coordinate
  */
 void Turn::setTarget(Coordinate target)
 {
 	m_target = target;
-}
-
-/**
- * @brief Returns if the object is fully initialized
- * @return true if the object is initialized
- */
-bool Turn::isInitialized() const
-{
-	return m_initialized;
-}
-
-/**
- * @brief Returns if the represented turn is valid or not
- * @return true if the turn is valid
- * @deprecated This function is replaced by Board::isValid(Turn)
- */
-bool Turn::isValid() const
-{
-	if(!isInitialized())
-		return false;
-	
-	///\todo Implement validation check
-	return true;
 }
 
 /**
@@ -102,11 +60,5 @@ int Turn::getId() const
  */
 Coordinate Turn::getTarget() const
 {
-	if(!isInitialized())
-	{
-		///\todo Do we want to use own Exception instances or any other error handler?
-		///\todo I start with this one.
-		throw "Turn is not fully initialized. No target coordinate is available.";
-	}
 	return m_target;
 }
--- Turn.hpp
+++ Turn.hpp
@@ -5,28 +5,21 @@
 
 class Turn {
 public:
-	Turn();
 	Turn(bool color, Coordinate target);
-	
-	bool isInitialized() const;
-	bool isValid() const;
 	
 	bool getColor() const;
 	int getId() const;
 	Coordinate getTarget() const;
 	
-	
-	static int instances;
-	
-	
-private:
 	void setColor(bool color);
 	void setTarget(Coordinate target);
-	void setInitialized();
+	
+	static int instances;
+private:
+	
 	
 	bool m_color = false;
 	int m_id = -1;
-	bool m_initialized = false;
 	Coordinate m_target;
 };
 
